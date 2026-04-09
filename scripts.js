7e4293fca0d1dca701dd5aa5d1daffe8575d87e0
Linus Tucker <172833324+MrStarfleetCommand@users.noreply.github.com>
2026-04-08 22:57:51 -0400

    Update to use new `fetchSourceText()` function from parser.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,6 @@
 /*
-import {txtToHtml, htmlToTxt} from '/parser.js';
+import {txtToHtml, htmlToTxt, fetchSourceText} from '/parser.js';
 (async () => {
-	const sourcePage = `${location.pathname.replace(/\/+$/, '')}/index.html`.replace(/\.html(?:\/index\.html)?$/, '.txt');
-	const txt = await (await fetch(sourcePage)).text();
-	const contentArea = document.createElement('div');
-	const pageHeading = document.createElement('h2');
-	const parserOutput = document.createElement('div');
-
-	pageHeading.innerText = document.title;
-	parserOutput.id = 'parser-output';
-	parserOutput.innerHTML = txtToHtml(txt);
-	contentArea.classList.add('content-area', 'scrollbox');
-	contentArea.appendChild(pageHeading);
-	contentArea.appendChild(parserOutput);
-	document.body.append(contentArea);
+	const sourceText = await fetchSourceText();
 })();
 */
