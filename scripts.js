66fe95363413e6e31b29e98d2d6156e5514cdd30
Linus Tucker <172833324+MrStarfleetCommand@users.noreply.github.com>
2026-04-10 09:54:26 -0400

    Implement parser.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
-/*
 import {txtToHtml, htmlToTxt, fetchSourceText} from '/parser.js';
 (async () => {
+	const heading = document.createElement('h1');
+	const parserOutput = document.createElement('div');
 	const sourceText = await fetchSourceText();
+
+	heading.append(document.title);
+	parserOutput.id = 'parser-output';
+	parserOutput.append(txtToHtml(sourceText));
+	document.body.append(heading, parserOutput);
 })();
-*/
