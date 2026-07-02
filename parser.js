555d5ffc86398c205782be65e3a11794000a3e6d
Linus Tucker <172833324+MrStarfleetCommand@users.noreply.github.com>
2026-07-02 18:34:00 -0400

    Begin parser rewrite
    The RegExp-based parsing currently in use has some limitations and drawbacks. This commit begins the process of switching to a different parsing model.

--- parser.js
+++ parser.js
@@ -1,19 +1,13 @@
 function txtToHtml(txt, pathPrefix){
-	let html = txt;
 	pathPrefix = pathPrefix ? pathPrefix.replace(/^\/*/, '/') : '';
-	html = html.trim();
-	html = html.replace(/</g, '&lt;');
+	txt = txt.trim();
+	txt = txt.replace(/</g, '&lt;');
+	processor(txt);
+	/*
 	html = html.replace(new RegExp(`^ *\\* *(?:\\{${htmlAttributes('value', 'class', 'id', 'title')} *\\} *)?(.*?[^\\s]) *$`, 'gmi'), '<li $1>$2</li>');
 	html = html.replace(new RegExp(`^\\{ *${htmlAttributes('class', 'id')} *\\n((?:<li .*?>.+</li>\\n*)+)\\n\\} *$`, 'gm'), '<ul $1>$2</ul>');
-	html = html.replace(/^(======) *(.+?) *\1 *$/gm, '<h6 class="heading">$2</h6>');
-	html = html.replace(/^(=====) *(.+?) *\1 *$/gm, '<h5 class="heading">$2</h5>');
-	html = html.replace(/^(====) *(.+?) *\1 *$/gm, '<h4 class="heading">$2</h4>');
-	html = html.replace(/^(===) *(.+?) *\1 *$/gm, '<h3 class="heading">$2</h3>');
-	html = html.replace(/^(==) *(.+?) *\1 *$/gm, '<h2 class="heading">$2</h2>');
 	html = html.replace(/&lt;!--([^]*?)-->/g, '<!--$1-->');
 	html = html.replace(/^ *([^<\s].*?) *$/gm, '<p>$1</p>');
-	html = html.replace(/''' *([^'].*?) *'''/g, '<span class="bold">$1</span>');
-	html = html.replace(/'' *([^'].*?) *''/g, '<span class="italic">$1</span>');
 	html = html.replace(/\s+/g, ' ');
 	html = html.split(/\[\[(.+?)\]\]/);
 
@@ -33,6 +27,7 @@ function txtToHtml(txt, pathPrefix){
 	html = html.join('');
 	html = html.replace(/\[ *([^\s"]+?) +(.+?) *\]/g, '<a href="$1" class="external-link">$2</a>');
 	return html;
+	*/
 }
 
 function htmlToTxt(html){
@@ -64,8 +59,102 @@ async function fetchSourceText(){
 	return await (await fetch(sourcePage)).text();
 }
 
+function processor(txt, html = '', isUnclosed = {}){
+	const c0 = txt[0];
+	const c1 = txt[1];
+	const c2 = txt[2];
+	const c3 = txt[3];
+	const c4 = txt[4];
+	const c5 = txt[5];
+	const c6 = txt[6];
+
+	if (c0 === '\n'){
+		isUnclosed.h6 = false;
+		isUnclosed.h5 = false;
+		isUnclosed.h4 = false;
+		isUnclosed.h3 = false;
+		isUnclosed.h2 = false;
+		isUnclosed.bold = false;
+		isUnclosed.italic = false;
+	}
+
+	if (/^`(?!`).+?(?<![`\\])`(?!`)/.test(txt)){
+		html += `<code class="markup">${decode(txt.replace(/^`(.+?)(?<![`\\])`(?!`)[^]*$/, '$1'))}</code>`;
+		txt = txt.replace(/^`.+?(?<![`\\])`(?!`)/, '');
+	} else if (/^```.+?(?<!\\)```/.test(txt)){
+		html += `<pre class="markup">${decode(txt.replace(/^```\s*(.+?)\s*(?<!\\)```[^]*$/, '$1'))}</pre>`;
+		txt = txt.replace(/^```.+?(?<!\\)```/, '');
+	} else if (c0 === '\n' && c1 === '=' && c2 === '=' && c3 === '=' && c4 === '=' && c5 === '=' && c6 === '='){
+		html += '<h6 class="heading">';
+		txt = txt.replace(/^\n====== */, '');
+		isUnclosed.h6 = true;
+	} else if (c0 === '=' && c1 === '=' && c2 === '=' && c3 === '=' && c4 === '=' && c5 === '=' && c6 === '\n' && isUnclosed.h6){
+		html += '</h6>';
+		txt = txt.replace(/^======/, '');
+		isUnclosed.h6 = false;
+	} else if (c0 === '\n' && c1 === '=' && c2 === '=' && c3 === '=' && c4 === '=' && c5 === '='){
+		html += '<h5 class="heading">';
+		txt = txt.replace(/^\n===== */, '');
+		isUnclosed.h5 = true;
+	} else if (c0 === '=' && c1 === '=' && c2 === '=' && c3 === '=' && c4 === '=' && c5 === '\n' && isUnclosed.h5){
+		html += '</h5>';
+		txt = txt.replace(/^=====/, '');
+		isUnclosed.h5 = false;
+	} else if (c0 === '\n' && c1 === '=' && c2 === '=' && c3 === '=' && c4 === '='){
+		html += '<h4 class="heading">';
+		txt = txt.replace(/^\n==== */, '');
+		isUnclosed.h4 = true;
+	} else if (c0 === '=' && c1 === '=' && c2 === '=' && c3 === '=' && c4 === '\n' && isUnclosed.h4){
+		html += '</h4>';
+		txt = txt.replace(/^====/, '');
+		isUnclosed.h4 = false;
+	} else if (c0 === '\n' && c1 === '=' && c2 === '=' && c3 === '='){
+		html += '<h3 class="heading">';
+		txt = txt.replace(/^\n=== */, '');
+		isUnclosed.h3 = true;
+	} else if (c0 === '=' && c1 === '=' && c2 === '=' && c3 === '\n' && isUnclosed.h3){
+		html += '</h3>';
+		txt = txt.replace(/^===/, '');
+		isUnclosed.h3 = false;
+	} else if (c0 === '\n' && c1 === '=' && c2 === '='){
+		html += '<h2 class="heading">';
+		txt = txt.replace(/^\n== */, '');
+		isUnclosed.h2 = true;
+	} else if (c0 === '=' && c1 === '=' && c2 === '\n' && isUnclosed.h2){
+		html += '</h2>';
+		txt = txt.replace(/^==/, '');
+		isUnclosed.h2 = false;
+	} else if (c0 === "'" && c1 === "'" && c2 === "'"){
+		if (isUnclosed.bold){
+			html += '</span>';
+			txt = txt.replace(/^'''/, '');
+			isUnclosed.bold = false;
+		} else {
+			html += '<span class="bold">';
+			txt = txt.replace(/^''' */, '');
+			isUnclosed.bold = true;
+		}
+	} else if (c0 === "'" && c1 === "'"){
+		if (isUnclosed.italic){
+			html += '</span>';
+			txt = txt.replace(/^''/, '');
+			isUnclosed.italic = false;
+		} else {
+			html += '<span class="italic">';
+			txt = txt.replace(/^'' */, '');
+			isUnclosed.italic = true;
+		}
+	}
+
+	processor(txt, html, isUnclosed);
+}
+
 function htmlAttributes(...allowList){
 	return `((?: *(?:${allowList.join('|')})(?: *= *".*?")?)*)`;
+}
+
+function decode(txt){
+	return txt.replace(/\\(.)/g, '$1');
 }
 
 export {txtToHtml, htmlToTxt, fetchSourceText};
